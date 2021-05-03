7106af1efc4f4226c75dffd1baedf35a4dac6070
Arthur Sidle <56030962+ArthurSidle@users.noreply.github.com>
2021-05-03 04:13:34 +0100

    changes to curtain.py (see below): curtain open/closed status now independent of time can now parse custom open/close times auto opens/closes based on these times

--- curtain.py
+++ curtain.py
@@ -2,21 +2,44 @@ import time
 
 class Curtain:
     OPEN=1
-    CLOSE=0
+    CLOSED=0
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
 
     def __init__(self):
         self.__current_time = self.current_time
         self.__time = self.t
-        self.__open_curtain = self.OPEN
-        self.__close_curtain = self.CLOSE
+        self.__status = self.CLOSED
+        self.__opening_time = self.current_time
+        self.__closing_time = self.current_time
 
     def run_curtain(self):
-        if self.__time.tm_hour > 7 and self.__time.tm_hour < 19:
-            return self.__open_curtain
-        elif self.__time.tm_hour < 7 and self.__time.tm_hour > 19:
-            return self.__close_curtain
+        if self.__time.tm_hour >= self.__opening_time.tm_hour
+        and self.__time.tm_hour < self.__closing_time.tm_hour
+        and self.__time.tm_min >= self.__opening_time.tm_min
+        and self.__time.tm_min < self.__closing_time.tm_min:
+            self.open_curtain()
+        else:
+            self.close_curtain()
+
+    def set_time_setting(self, setting):
+        time_range = setting.split("-")
+
+        if len(time_range) != 2:
+            return False
+            
+        try:
+            self.__opening_time = time.strptime(time_range[0], "%H %M")
+            self.__closing_time = time.strptime(time_range[1], "%H %M")
+        except ValueError:
+            return False
+
 
     def get_time(self):
         return self.__current_time
+    
+    def open_curtain(self):
+        self.__status = self.OPEN
+    
+    def close_curtain(self):
+        self.__status = self.CLOSED
